f89f5c44fb96c8e8e640bea7ee63e85f751fe092
Yazdan Aghajanian <aghajaniaan@gmail.com>
2026-02-23 13:21:36 +1030

    Add Bronze layer structure

---
+++ bronze/ingestion_notebook.py
@@ -0,0 +1,8 @@
+"""
+Bronze Layer - Raw Data Ingestion
+
+This module simulates raw source ingestion into the Bronze layer.
+"""
+
+def ingest_raw_data():
+    print("Ingesting raw data into Bronze layer")
